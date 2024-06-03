083200712f917eed845a66c2f35382a59f3030f1
GitLab CI pipeline <info@videoexpertsgroup.com>
2024-06-03 13:58:21 +0000

    commited by GitLab CI for commit 6197d317a5fcec0e15ff8eb60c4d7aad82b20bd1

--- core/vxg_cloudone_api.js
+++ core/vxg_cloudone_api.js
@@ -499,10 +499,10 @@ vxg.api.cloudone.camera.update = function(obj){
 
 vxg.api.cloudone.camera.del = function(cameraID, gatewayInfo = null){
     var data = {id: cameraID};
-    if (gatewayInfo.gatewayUrl) data.gatewayUrl = gatewayInfo.gatewayUrl;
-    if (gatewayInfo.gatewayId) data.gatewayId = gatewayInfo.gatewayId;
-    if (gatewayInfo.gatewayUsername) data.gatewayUsername = gatewayInfo.gatewayUsername;
-    if (gatewayInfo.gatewayPassword) data.gatewayPassword = gatewayInfo.gatewayPassword;  
+    if (gatewayInfo?.gatewayUrl) data.gatewayUrl = gatewayInfo.gatewayUrl;
+    if (gatewayInfo?.gatewayId) data.gatewayId = gatewayInfo.gatewayId;
+    if (gatewayInfo?.gatewayUsername) data.gatewayUsername = gatewayInfo.gatewayUsername;
+    if (gatewayInfo?.gatewayPassword) data.gatewayPassword = gatewayInfo.gatewayPassword;  
 //    data.uid = vs_api.uid;
 //    if (vs_api.manage_uid)
 //        data.manage_uid = vs_api.manage_uid;
